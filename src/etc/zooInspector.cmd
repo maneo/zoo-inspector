f0e4783c73dc71a80c415d230319050e7bb42697
Adam Dudczak <adam.dudczak@allegro.pl>
2014-08-27 23:50:14 +0200

    working distro

--- src/etc/zooInspector.cmd
+++ src/etc/zooInspector.cmd
@@ -15,4 +15,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-java -jar {project.build.finalName}.jar
+java -jar ${project.build.finalName}.jar
